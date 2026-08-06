0b33f7020428d2dd29e5d8e2c086587cdcb325ff
kamalkr1506-beep <kamalkr1506@gmail.com>
2026-08-06 12:02:47 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('erhw-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
